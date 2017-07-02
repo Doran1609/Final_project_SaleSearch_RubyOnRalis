def7a2b6b2637e3c37abe4087a676ecc4400abef
Dorancelis Cordones <doran162@hotmail.com>
2017-07-02 18:53:15 +0000

    Project 02/07/2017

---
+++ app/models/company.rb
@@ -0,0 +1,5 @@
+class Company < ActiveRecord::Base
+    validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 50 }
+    validates :image, presence: true, length: { minimum: 3, maximum: 50 }
+end
