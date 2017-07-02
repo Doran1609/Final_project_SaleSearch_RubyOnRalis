0442e245ea1c8889d4dc4dc22b82ce3a0aa1acd6
Dorancelis Cordones <doran162@hotmail.com>
2017-07-02 21:14:19 +0000

    Creating an Item but with an error when I want to edit/update

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ActiveRecord::Base
     validates :name, presence: true, length: { minimum: 3, maximum: 50 }
     validates :description, presence: true, length: { minimum: 10, maximum: 50 }
     validates :image, presence: true, length: { minimum: 3, maximum: 50 }
+    has_many :items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   get 'items/update'
 
-  resources :companies
+  resources :companies do
+    resources :items
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
